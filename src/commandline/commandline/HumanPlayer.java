51a89ba8d41a43bd20cfd4c2a27482da5a94e2d4
marthagarman1 <46495582+marthagarman1@users.noreply.github.com>
2019-01-23 16:55:55 +0000

    Create HumanPlayer.java

---
+++ src/commandline/commandline/HumanPlayer.java
@@ -0,0 +1,11 @@
+package commandline;
+import java.util.ArrayList;
+
+public class HumanPlayer extends Player {
+   
+   public HumanPlayer (ArrayList<Card> deck) {
+      super(deck);
+   }
+   
+   public void selectCard() {}
+}
