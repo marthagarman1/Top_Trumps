350b32b9f7e0e4bf4fd6dabae502e4e579339e41
marthagarman1 <46495582+marthagarman1@users.noreply.github.com>
2019-01-23 21:23:01 +0000

    Update AIPlayer.java

--- src/commandline/commandline/AIPlayer.java
+++ src/commandline/commandline/AIPlayer.java
@@ -1,15 +1,19 @@
 package commandline;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class AIPlayer extends Player {
     
-   
+   //ArrayList<Card> deck = new ArrayList<>();
    public AIPlayer(ArrayList<Card> deck) {
       super(deck); 
    }
    
-   public void selectCard() {
-   
+   public Card selectCard() {
+      Random r = new Random(); 
+      int index = r.nextInt(deck.size());
+      Card drawn = deck.get(index);
+      return drawn; 
    }
    
 
