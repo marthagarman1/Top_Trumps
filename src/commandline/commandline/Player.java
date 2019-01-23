e6f52ceb5bf6f9ee9fd5b3a15a958685e2f2d9e8
marthagarman1 <46495582+marthagarman1@users.noreply.github.com>
2019-01-23 21:20:22 +0000

    Update Player.java

--- src/commandline/commandline/Player.java
+++ src/commandline/commandline/Player.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 
 public abstract class Player {
    ArrayList<Card> deck = new ArrayList<Card>();
+   
    public Player(ArrayList<Card> deck) {
       this.deck = deck; 
    }
    
-   public abstract void selectCard();
-   
+      
    public String printDeck() {
       String output = deck.toString(); 
       return output;
    }; 
+   
+   public void setPlayerNumber() {
+      
+   } 
+   
+   
 }
