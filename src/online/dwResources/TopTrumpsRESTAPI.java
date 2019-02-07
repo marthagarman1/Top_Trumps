afb9294052fe3b88932dc126e2ac4fafffe7a7bc
adrianaandriolar <46526287+adrianaandriolar@users.noreply.github.com>
2019-02-07 11:03:30 +0000

    Add files via upload

--- src/online/dwResources/TopTrumpsRESTAPI.java
+++ src/online/dwResources/TopTrumpsRESTAPI.java
@@ -1,5 +1,6 @@
 package online.dwResources;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +14,15 @@ import javax.ws.rs.core.MediaType;
 
 import online.configuration.TopTrumpsJSONConfiguration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+
+import commandline.Card;
+import commandline.HumanPlayer;
+import commandline.ImportDeckInformation;
+import commandline.Player;
+import commandline.TopTrumpsCLIApplication;
 
 @Path("/toptrumps") // Resources specified here should be hosted at http://localhost:7777/toptrumps
 @Produces(MediaType.APPLICATION_JSON) // This resource returns JSON content
@@ -29,11 +37,14 @@ import com.fasterxml.jackson.databind.ObjectWriter;
  * REST API methods in Dropwizard. You will need to replace these with
  * methods that allow a TopTrumps game to be controled from a Web page.
  */
+
 public class TopTrumpsRESTAPI {
 
 	/** A Jackson Object writer. It allows us to turn Java objects
 	 * into JSON strings easily. */
 	ObjectWriter oWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
+	
+	private String deckFile;
 	
 	/**
 	 * Contructor method for the REST API. This is called first. It provides
@@ -42,6 +53,7 @@ public class TopTrumpsRESTAPI {
 	 * @param conf
 	 */
 	public TopTrumpsRESTAPI(TopTrumpsJSONConfiguration conf) {
+	
 		// ----------------------------------------------------
 		// Add relevant initalization here
 		// ----------------------------------------------------
@@ -83,5 +95,118 @@ public class TopTrumpsRESTAPI {
 	public String helloWord(@QueryParam("Word") String Word) throws IOException {
 		return "Hello "+Word;
 	}
-	
+	@GET
+    @Path("/start")
+
+	/** Creates a new game **/
+    public TopTrumpsCLIApplication startGame() {
+        TopTrumpsCLIApplication game = null;
+        ImportDeckInformation game1 = null;
+		if (game.getRoundCount() == 0) {
+            game1.getDeck();
+        }
+        return game;
+    }
+
+    @GET
+    @Path("/restart")
+	/** Restarts the game **/
+    public TopTrumpsCLIApplication restart() throws FileNotFoundException {
+    	 TopTrumpsCLIApplication game;
+         ImportDeckInformation game1 = null;
+    
+        game = new TopTrumpsCLIApplication();
+        game1.getDeck();
+        game.getNumOfPlayers();
+        return game;
+    }
+
+    @GET
+    @Path("/play")
+	/** Handles a single round
+	 * if it is human's turn it accepts the category passed,
+	 * unless the human player has been eliminated
+	 * else it takes an AI's chosen category and plays the round;
+	 * Accordingly detects the round winner and informs if a player has been eliminated
+	 * **/
+   /* public TopTrumpsCLIApplication playRoundWithCategory(@QueryParam("category") int category) {
+        TopTrumpsCLIApplication game;
+    	int roundCategory;
+    	
+        if (game.getActivePlayer() == 0) {
+            roundCategory = category;
+        } else {
+            roundCategory = game.catChoice();
+        }
+
+        int rndWinner = game.playRound(roundCategory);
+
+        if (rndWinner != -1) {
+            game.winnerTakeCards(rndWinner);
+        }
+
+        game.removeEliminatedPlayers();
+
+        //If game has finished write results to database
+        if (game.isFinished()) {
+        	Card card = new Card(deckFile, null, null);
+            Database db = new Database();
+            db.connect();
+            db.insertData(game.getRoundCount(), game.getStats(), game.getActivePlayer());
+            db.disconnect();
+        }
+
+        return game;
+        */
+    
+
+  //  @GET
+  //  @Path("/game")
+	/** Initialises a new game **/
+    public TopTrumpsCLIApplication game() {
+        return game();
+    }
+
+    //@GET
+   // @Path("/categoryValues")
+	/** Converts the array of categories into a JSON string **/
+    //public String categoryValues() throws JsonProcessingException {
+        TopTrumpsCLIApplication game = new TopTrumpsCLIApplication();
+        Card card = new Card(deckFile, null, null);
+	//	//String categories = oWriter.writeValueAsString(game.getStats(););
+        //return categories;        
+    
+    @GET
+    @Path("/database")
+	/** Converts the array of categories into a JSON string **/
+    public String databaseValues() throws JsonProcessingException {
+    	
+    	System.out.println("data base working");
+    
+    	/*Database a = new Database();
+    	ArrayList<Integer> Values = new ArrayList<Integer>();
+		a.connect();
+		Map<String, String> gameStats = new HashMap<String , String>();
+        gameStats.put("gamesPlayed", ""+a.getGamesPlayed());
+        gameStats.put("HumanWins", ""+a.getHumanWins() );
+        gameStats.put("ComputerWins", ""+a.getComputerWins());
+        gameStats.put("AverageDraws", ""+a.getAverageDraws());
+        gameStats.put("MaxRounds", ""+a.getMaxRounds());
+        a.disconnect();
+
+        // Convert a Map into JSON string.
+        Gson gson = new Gson();
+        String json = gson.toJson(gameStats);
+*/
+        return "{}";
+    }
 }
+
+
+
+
+
+
+
+
+
