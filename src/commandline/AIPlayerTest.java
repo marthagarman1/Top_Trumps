8d6696a97c6c10c5255032072704302cad38ec72
marthagarman1 <46495582+marthagarman1@users.noreply.github.com>
2019-02-16 11:38:47 +0000

    Added classpath and library

--- src/commandline/AIPlayerTest.java
+++ src/commandline/AIPlayerTest.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AIPlayerTest {
